5b0c73c23e4e751db7c3f33c70422201b5e7e1e9
Gavin_kang <776362265@qq.com>
2021-05-04 10:03:01 +0800

    初始化项目

---
+++ MultithreadingDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MultithreadingDemo
+//
+//  Created by ZKKMBP on 2021/5/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MultithreadingDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MultithreadingDemo
+//
+//  Created by ZKKMBP on 2021/5/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
